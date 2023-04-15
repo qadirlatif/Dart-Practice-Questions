40664b8d91c08c4bd01dcb9bba71d14799b62caf
Abdul Qadir Abdul Latif <92687658+qadirlatif@users.noreply.github.com>
2023-04-15 17:11:39 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,22 @@
+void main() {
+  String userinput = "444558744411225";
+  String finalresult = "";
+  int times = 0;
+  for (int count = 0; count < userinput.length; count++) {
+    for (int count_2 = 0; count_2 < userinput.length; count_2++) {
+      if (userinput[count] == userinput[count_2]) {
+        ++times;
+      }
+    }
+    if (count == 0) {
+      finalresult =
+          finalresult + userinput[count].toString() + times.toString();
+    } else if (userinput[count] != userinput[count - 1]) {
+      finalresult =
+          finalresult + userinput[count].toString() + times.toString();
+    }
+    times = 0;
+  }
+  print(finalresult);
+}
 
